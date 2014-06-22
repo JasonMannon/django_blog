ab780463c5a8e0b6c265e649f8a1c30d59006719
Jason Mannon <jmannonpdx@gmail.com>
2014-06-22 17:59:28 +0000

    added psycopg2 and dj_database_url

---
+++ microblog/settings/base.py
@@ -0,0 +1,103 @@
+"""
+Django settings for microblog project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+import os
+
+import dj_database_url
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'p1205&sy(ja06dsd&vb3(q!3f5c0%f4873cfthi+7f488wx_qj'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+)
+
+THIRT_PARTY_APPS = ()
+
+LOCAL_APPS = ()
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'microblog.urls'
+
+WSGI_APPLICATION = 'microblog.wsgi.application'
+
+MEDIA_ROOT = root("..", "uploads")
+
+MEDIA_URL = ''
+
+STATIC_ROOT = root("..", "static")
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config()
+    }
+
+
+STATIC_FILE_DIRS = (
+    root("assets")
+)
+
+TEMPLATE_DIRS = (
+    root("templates")
+)
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'America/Los_Angeles'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ microblog/settings/local.py
@@ -0,0 +1,9 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'microblog',
+    }
+}
